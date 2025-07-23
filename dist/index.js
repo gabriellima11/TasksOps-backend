adca4fea06cc9cde02d334ae87d90c322e046986
Gabriel Lima <gabriel.lima@southti.local>
2025-07-23 10:09:30 -0300

    fix index.ts

--- dist/index.js
+++ dist/index.js
@@ -9,16 +9,18 @@ const express_1 = __importDefault(require("express"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const routes_1 = __importDefault(require("./routes/routes"));
 dotenv_1.default.config();
-const MONGO = process.env.MONGO_CONNECTION || '';
+const PORT = process.env.PORT;
+const MONGO = process.env.MONGO_CONNECTION || "";
 const app = (0, express_1.default)();
+const port = 3000;
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
-app.use('/api', routes_1.default);
-if (mongoose_1.default.connection.readyState === 0) {
-    mongoose_1.default
-        .connect(MONGO)
-        .then(() => console.log('MongoDB conectado'))
-        .catch((err) => console.error('Erro ao conectar no MongoDB:', err));
-}
-// Exporta o app para o Vercel usar como função serverless
-exports.default = app;
+// Rotas base
+app.use("/api", routes_1.default);
+mongoose_1.default.connect(MONGO).then(() => {
+    app.listen(port, '0.0.0.0', () => {
+        console.log(`Servidor iniciado na porta ${port}`);
+    });
+}).catch(err => {
+    console.error("Erro ao conectar no MongoDB:", err);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,28 @@
-import dotenv from 'dotenv';
-import cors from 'cors';
-import express from 'express';
-import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+import cors from 'cors'
+import express from 'express'
+import mongoose from 'mongoose'
 
-import Routes from './routes/routes';
+import Routes from './routes/routes'
 
-dotenv.config();
+dotenv.config()
 
-const MONGO = process.env.MONGO_CONNECTION || '';
-const app = express();
+const PORT = process.env.PORT
+const MONGO = process.env.MONGO_CONNECTION || ""
+const app = express()
+const port = 3000
+app.use(cors())
+app.use(express.json())
 
-app.use(cors());
-app.use(express.json());
-app.use('/api', Routes);
+// Rotas base
+app.use("/api", Routes);
 
-if (mongoose.connection.readyState === 0) {
-  mongoose
-    .connect(MONGO)
-    .then(() => console.log('MongoDB conectado'))
-    .catch((err) => console.error('Erro ao conectar no MongoDB:', err));
-}
+mongoose.connect(MONGO).then(() => {
+    app.listen(port, '0.0.0.0', () => {
+        console.log(`Servidor iniciado na porta ${port}`)
+    })
+}).catch(err => {
+    console.error("Erro ao conectar no MongoDB:", err);
+  });
 
-// Exporta o app para o Vercel usar como função serverless
-export default app;
+
