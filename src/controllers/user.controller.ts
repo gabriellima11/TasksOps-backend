0685be8d563f9372bc74957627e5355c231acb41
Gabriel Lima <gabriel.lima@southti.local>
2025-07-28 11:25:42 -0300

    :sparkles: feat: add login and register

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -42,6 +42,7 @@ export const login = async (req: Request, res: Response) => {
         id: user._id,
         name: user.name,
         email: user.email,
+        admin: user.admin
       }
     });
   } catch (error) {
